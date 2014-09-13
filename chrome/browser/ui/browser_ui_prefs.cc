3719fbd66e4c55de0072e014e5dc44db6026cc96
Jan Engelhardt <jengelh@inai.de>
2014-09-13 16:55:12 +0200

    prefs: disable Cloud Print
    Cloud Print is usually autodisabled if no printers are shared.
    So what exactly am I turning off?
    
    Probably also needs to gain a checkbox in the settings.

--- chrome/browser/ui/browser_ui_prefs.cc
+++ chrome/browser/ui/browser_ui_prefs.cc
@@ -113,7 +113,7 @@ void RegisterBrowserUserPrefs(user_prefs::PrefRegistrySyncable* registry) {
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
   registry->RegisterBooleanPref(
       prefs::kCloudPrintSubmitEnabled,
-      true,
+      false,
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
   registry->RegisterBooleanPref(
       prefs::kDevToolsDisabled,
