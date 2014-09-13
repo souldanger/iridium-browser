f592b04f35cb0d8b89f78661ddc6c4a2390ce531
Jan Engelhardt <jengelh@inai.de>
2014-09-13 17:12:43 +0200

    first_run: deactivate autoupdate globally
    We currently do not have any update service infrastructure in place
    (i.e. on our webserver), and sending update requests to Google also
    sounds meh (wrong provider, after all).
    
    Also, there is no tunable in chrome://settings (or similar) yet to
    turn it back on.

--- chrome/browser/extensions/extension_system_impl.cc
+++ chrome/browser/extensions/extension_system_impl.cc
@@ -188,6 +188,9 @@ void ExtensionSystemImpl::Shared::Init(bool extensions_enabled) {
 
   bool autoupdate_enabled = !profile_->IsGuestSession() &&
                             !profile_->IsSystemProfile();
+  if (autoupdate_enabled)
+    fprintf(stderr, "*** autoupdate was enabled, overriding with false\n");
+  autoupdate_enabled = false;
 #if defined(OS_CHROMEOS)
   if (!extensions_enabled)
     autoupdate_enabled = false;
