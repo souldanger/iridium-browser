e74bf6164e89d96dc154b69c60852c7d965cdba9
Jan Engelhardt <jengelh@inai.de>
2014-09-13 16:40:17 +0200

    first_run: barf if metrics_reporting is activated
    This patch is for debugging purposes and is meant for discovering
    whether — despite our patching efforts — metrics_reporting is still
    enabled somehow, and if so, say so on stderr.
    
    Result: As of October 2014, metrics_reporting seems successfully
    disabled, as no message is emitted.

--- chrome/browser/first_run/first_run_internal_posix.cc
+++ chrome/browser/first_run/first_run_internal_posix.cc
@@ -17,6 +17,7 @@
 #include "components/metrics/metrics_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/startup_metric_utils/browser/startup_metric_utils.h"
+#include <cstdio>
 
 namespace first_run {
 namespace internal {
@@ -37,6 +38,7 @@ void DoPostImportPlatformSpecificTasks(Profile* profile) {
   // the pref (on Windows, the download is tagged with enable/disable stats so
   // this is POSIX-specific).
   if (GoogleUpdateSettings::GetCollectStatsConsent()) {
+    fprintf(stderr, "*** metrics_reporting = 1\n");
     g_browser_process->local_state()->SetBoolean(
         metrics::prefs::kMetricsReportingEnabled, true);
   }
