b834b585832a9be1f087fcf6b251a0e8923ea754
Jan Engelhardt <jengelh@inai.de>
2014-09-13 17:06:40 +0200

    prefs: do not store passwords by default

--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -137,7 +137,7 @@ void PasswordManager::RegisterProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(
       prefs::kPasswordManagerSavingEnabled,
-      true,
+      false,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterBooleanPref(
       prefs::kCredentialsEnableService, true,
