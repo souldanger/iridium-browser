bff610e74f6f3fc640a521e8254c8ff6bf62957a
Jan Engelhardt <jengelh@inai.de>
2014-10-13 12:28:51 +0200

    common: change the User-Agent string to read Iridium
    Continue providing Chrome/* for possible compatibility checks by the browser
    and/or websites, since we really are still Chromium.

--- content/common/user_agent.cc
+++ content/common/user_agent.cc
@@ -191,6 +191,8 @@ std::string BuildUserAgentFromOSAndProduct(const std::string& os_info,
       product.c_str(),
       WEBKIT_VERSION_MAJOR,
       WEBKIT_VERSION_MINOR);
+  if (strncmp(product.c_str(), "Iridium/", 8) == 0)
+  	base::StringAppendF(&user_agent, " Chrome/40.0.2214.93");
   return user_agent;
 }
 
