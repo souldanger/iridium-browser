625d631d8df6a51c5a5aa571f18a37415b3f7c0b
Jan Engelhardt <jengelh@inai.de>
2015-06-02 11:01:50 +0200

    updater: disable updater pings
    Despite auto-updater being arguably disabled (see previous commit),
    Chromium would still send background requests. Kill it.
    (trk:170, trk:171)

--- chrome/browser/component_updater/component_updater_url_constants.cc
+++ chrome/browser/component_updater/component_updater_url_constants.cc
@@ -7,13 +7,13 @@
 namespace component_updater {
 
 // The alternative URL for the v3 protocol service endpoint.
-const char kUpdaterAltUrl[] = "http://clients2.google.com/service/update2";
+const char kUpdaterAltUrl[] = "trk:170:http://clients2.google.com/service/update2";
 
 // The default URL for the v3 protocol service endpoint. In some cases, the
 // component updater is allowed to fall back to and alternate URL source, if
 // the request to the default URL source fails.
 // The value of |kDefaultUrlSource| can be overridden with
 // --component-updater=url-source=someurl.
-const char kUpdaterDefaultUrl[] = "https://clients2.google.com/service/update2";
+const char kUpdaterDefaultUrl[] = "trk:171:https://clients2.google.com/service/update2";
 
 }  // namespace component_updater
