cfa60043edb018eeb746d4d99b4e5e5787eace7d
Joachim Bauch <jojo@struktur.de>
2015-07-07 17:02:09 +0200

    safe_browsing: disable incident reporting
    Disables the safebrowsing incident reporting where you could upload
    information about a blocked URL to Google (also added a trk prefix to
    the URL so we get notified if this happens again in the future).

--- chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.cc
+++ chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.cc
@@ -287,6 +287,8 @@ IncidentReportingService::UploadContext::~UploadContext() {
 
 // static
 bool IncidentReportingService::IsEnabledForProfile(Profile* profile) {
+  return false;
+#if 0
   if (profile->IsOffTheRecord())
     return false;
   if (!profile->GetPrefs()->GetBoolean(prefs::kSafeBrowsingEnabled))
@@ -295,6 +297,7 @@ bool IncidentReportingService::IsEnabledForProfile(Profile* profile) {
     return true;
   return profile->GetPrefs()->GetBoolean(
       prefs::kSafeBrowsingExtendedReportingEnabled);
+#endif
 }
 
 IncidentReportingService::IncidentReportingService(
--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -399,9 +399,11 @@ SafeBrowsingService::CreateIncidentReportingService() {
 
 void SafeBrowsingService::RegisterAllDelayedAnalysis() {
 #if defined(FULL_SAFE_BROWSING)
+#if 0
   RegisterBinaryIntegrityAnalysis();
   RegisterBlacklistLoadAnalysis();
   RegisterVariationsSeedSignatureAnalysis();
+#endif
 #else
   NOTREACHED();
 #endif
